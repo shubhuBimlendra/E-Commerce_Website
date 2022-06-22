a570252fd5b30bd657b121af9237149b50fbe146
Bimlendra <bimlendrayadav2075@gmail.com>
2022-06-22 15:55:52 +0545

    Item increased Decreased Remove all function has been implemented

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,41 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+//Import
+use Cart;
 
 class CartComponent extends Component
 {
+    //Creating function for increasing product
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    // Creating function for Decreasing product
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+    }
+
+    // Creating function to remove or delete product from the cart
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+        session()->flash('success_message','Item has been removed');
+    }
+
+    //Creating a function clear all item from the cart
+    public function destroyAll()
+    {
+        Cart::destroy();
+    }
+
+
     public function render()
     {
         return view('livewire.cart-component')->layout("layouts.base");
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -31,14 +31,14 @@
                     <div class="price-field produtc-price"><p class="price">{{$item->model->regular_price}}</p></div>
                     <div class="quantity">
                         <div class="quantity-input">
-                            <input type="text" name="product-quatity" value="{{$item->model->qty}}" data-max="120" pattern="[0-9]*" >
-                            <a class="btn btn-increase" href="#"></a>
-                            <a class="btn btn-reduce" href="#"></a>
+                            <input type="text" name="product-quatity" value="{{$item->qty}}" data-max="120" pattern="[0-9]*" >
+                            <a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$item->rowId}}')"></a>
+                            <a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{$item->rowId}}')"></a>
                         </div>
                     </div>
                     <div class="price-field sub-total"><p class="price">{{$item->model->subtotal}}</p></div>
                     <div class="delete">
-                        <a href="#" class="btn btn-delete" title="">
+                        <a href="#" class="btn btn-delete" title="" wire:click.prevent="destroy('{{$item->rowId}}')">
                             <span>Delete from your cart</span>
                             <i class="fa fa-times-circle" aria-hidden="true"></i>
                         </a>
@@ -67,7 +67,7 @@
                 <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
             </div>
             <div class="update-clear">
-                <a class="btn btn-clear" href="#">Clear Shopping Cart</a>
+                <a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()">Clear Shopping Cart</a>
                 <a class="btn btn-update" href="#">Update Shopping Cart</a>
             </div>
         </div>

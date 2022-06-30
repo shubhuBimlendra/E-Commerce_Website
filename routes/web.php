52f3e87dd69e75908c43d158d9b4b5ee2253ff69
Bimlendra <bimlendrayadav2075@gmail.com>
2022-06-30 18:30:31 +0545

    Admin-Add-New-Product

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 
 
 /*
@@ -81,6 +82,7 @@ Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function(){
     Route::get('admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
     Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
     Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+    Route::get('admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
 });
 
 
